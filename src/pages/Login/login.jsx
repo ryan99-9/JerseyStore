b053f94e1ba497874b5527112f5f37958dc7eca7
unknown <edoafrian90@gmail.com>
2022-04-16 16:33:10 +0700

    Login

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,18 +1,39 @@
+import axios from 'axios'
 import React from 'react'
 
-import { InputGroup, FormControl, Button } from 'react-bootstrap'
+import { InputGroup, FormControl, Button, Modal } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import './Login.css';
+import './Login.css'
 
 class Login extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            visibility: false
+            visibility: false,
+            error: false,
+            errorLogin: false
         }
     }
+    onLogin = () => {
+        //ambil data dari username dan password
+        let username = this.refs.username.value
+        let password = this.refs.password.value
+
+        //Kalau ada input yang masih kosong, dikasih notif jika tidak boleh ada yang kosong
+        if (!username || !password) {
+            return this.setState({error:true})
+        }
+        //Cek apakah data username dan password yang di input user/diambil sudah ada didatabase 
+        axios.get(`http://localhost:2000/users?username=${username}&password=${password}`)
+        .then(res=>{
+            if(res.data.length === 0) {
+                return this.setState({error:true})
+            }
+        })
+
+    }
     render() {
-        const {visibility} = this.state
+        const { visibility } = this.state
         return (
             <div className='bg'>
                 <div className='contForm'>
@@ -24,25 +45,49 @@ class Login extends React.Component {
                         </InputGroup.Text>
                         <FormControl
                             placeholder="Input Here"
+                            ref="username"
                         />
                     </InputGroup>
                     <label className='textLabel'>Password</label>
                     <InputGroup className="mb-3">
                         <InputGroup.Text id="basic-addon1" onClick={() => this.setState({ visibility: !visibility })}>
                             {visibility ?
-                               <i class="fas fa-eye"></i> : <i class="fas fa-eye-slash"></i>                                
+                                <i class="fas fa-eye"></i> : <i class="fas fa-eye-slash"></i>
                             }
 
                         </InputGroup.Text>
                         <FormControl
                             placeholder="Input Here"
+                            ref="password"
                             type={visibility ? "text" : "password"}
                         />
                     </InputGroup>
-                    <Button className='loginButton'>Login in</Button>
+                    <Button onClick={this.onLogin} className='loginButton'>Login in</Button>
                     <p className='textHaveAccount'>Do you have an account?
                         <Link to="/Register" className='textRegister'>Register
                         </Link></p>
+                    <Modal show={this.state.error}>
+                        <Modal.Header>
+                            <Modal.Title>Error</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>Masukan Data Dengan Benar</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=> this.setState({error:false})} variant="secondary">Close</Button>
+                        </Modal.Footer>
+                    </Modal>
+                    <Modal show={this.state.errorLogin}>
+                        <Modal.Header>
+                            <Modal.Title>Error</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>This account isn't exist!</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={()=> this.setState({errorLogin:false})} variant="secondary">Close</Button>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
         )
