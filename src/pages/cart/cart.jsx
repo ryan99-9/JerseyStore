e204ce13aa9afa45a5da4d3d1100a51235ba535f
unknown <edoafrian90@gmail.com>
2022-05-10 09:58:36 +0700

    cartmodel

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,13 +2,68 @@ import React from 'react'
 // import Axios from 'axios'
 import { connect } from 'react-redux'
 import './cart.css'
+import { Button, Table } from 'react-bootstrap'
+import { Navigate } from 'react-router-dom'
 
 class Cart extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             cart: [],
+
         }
+    }
+    showTHead = () => {
+        return (
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th></th>
+                </tr>
+            </thead>
+        )
+    }
+    showTBody = () => {
+        const { userCart } = this.props
+        return (
+            <tbody>
+                {userCart.map((item, index) => {
+                    return (
+                        <tr>
+                            <td>
+                                {/* <div className='layerImg'> */}
+                                <img
+                                    className='contImg'
+                                    src={item.image}
+                                    alt='product' />
+                            {/* </div> */}
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.quantity}</td>
+                            <td>Rp {item.price.toLocaleString()}</td>
+                            <td>edit , delete</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+            // <div className='layerDisplay'>
+
+            //     <div className='layerText'>
+            //         <p>Product Name : {item.name}</p>
+            //         <p>Brand : {item.brand}</p>
+            //         <div style={{ display: 'flex' }}>
+            //             <p>Quantity : {item.quantity}</p>
+            //             <Button onClick={() => this.onEdit}>Edit</Button>
+            //         </div>
+            //         <p>Price : Rp{(item.price * item.quantity).toLocaleString()} </p>
+            //     </div>
+            // </div>     
+
+        )
+
     }
     // componentDidMount() {
     //     let id = this.props.userId
@@ -22,42 +77,30 @@ class Cart extends React.Component {
     // }
 
     render() {
+        if (!this.props.userName) {
+            return <Navigate to="/Login" />
+        }
         // const { cart } = this.state
         // console.log(cart);
-        const {userCart} = this.props
         return (
             <>
-            <div className='cart'>Your Cart</div>
-            <div className='alltrans'>Pilih Semua</div>
-
-            {userCart.map(item=>{
-                return(
-                     <div className='layerDisplay'>
-                    <div className='layerImg'>
-                        <img 
-                        className='contImg'
-                        src={item.image} 
-                        alt='product' />
-                    </div>
-                    <div className='layerText'>
-                        <p>Product Name : {item.name}</p>
-                        <p>Brand : {item.brand}</p>
-                        <p>Quantity : {item.quantity}</p>
-                        <p>Price : Rp{(item.price*item.quantity).toLocaleString()} </p>
-                    </div>
-                </div>
-                )
-            })}
-
-          
-               
+                <div className='cart'>Your Cart</div>
+                <div className='alltrans'>Pilih Semua</div>
+                <br />
+                <Table striped bordered hover>
+                    {this.showTHead()}
+                    {this.showTBody()}
+                </Table>
             </>
         )
     }
 }
+
+
 const mapStateToProps = (take) => {
     return {
-        userCart: take.userReducer.cart
+        userCart: take.userReducer.cart,
+        userName: take.userReducer.username,
     }
 }
 export default connect(mapStateToProps)(Cart)
